79c413d32a986891b9153d4b6a01c8f11fc217d6
chittiprolu, sitara - Contractor <schittiprolu@contractor.ea.com>
2017-12-05 21:08:32 -0600

    Added binaries for Windows

---
+++ src/test/java/org/sample/browser/DriverManager.java
@@ -0,0 +1,31 @@
+package org.sample.browser;
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class DriverManager {
+
+    protected WebDriver driver;
+
+    protected abstract void createDriver();
+
+    protected abstract void startService();
+
+    protected abstract void stopService();
+
+
+    public WebDriver getDriver() {
+        if (driver == null) {
+            startService();
+            createDriver();
+        }
+        return driver;
+    }
+
+    public void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+}
