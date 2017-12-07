17eb65edb4efe08ef8d0976abed688e26ceb30fd
chittiprolu, sitara - Contractor <schittiprolu@contractor.ea.com>
2017-12-06 20:26:56 -0600

    Corrected Page initialization

--- src/test/java/org/eaworld/tests/EAWorldLoginPageTest.java
+++ src/test/java/org/eaworld/tests/EAWorldLoginPageTest.java
@@ -1,14 +1,17 @@
 package org.eaworld.tests;
 
+import org.eaworld.pageimpl.MainPageImpl;
+import org.eaworld.pages.EAWorldPage;
+import org.eaworld.pages.MainPage;
 import org.testng.annotations.Test;
 
 import com.google.inject.Inject;
 
 import org.eaworld.pageimpl.EAWorldHomePageImpl;
+import org.testng.internal.BaseTestMethod;
 
 
-
-public class EAWorldLoginPageTest extends BaseTest {
+public class EAWorldLoginPageTest {
 
     @Inject
     EAWorldHomePageImpl eaWorldHomePageImpl;
