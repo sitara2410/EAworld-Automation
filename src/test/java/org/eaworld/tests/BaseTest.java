7499d303f7f251285f863c1dd5dfab61f047b4cf
chittiprolu, sitara - Contractor <schittiprolu@contractor.ea.com>
2017-12-06 20:57:45 -0600

    Added BaseTest.class

---
+++ src/test/java/org/eaworld/tests/BaseTest.java
@@ -0,0 +1,25 @@
+package org.eaworld.tests;
+
+import com.google.inject.Inject;
+import org.eaworld.pageimpl.MainPageImpl;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    @Inject
+    MainPageImpl mainPage;
+
+    @BeforeMethod(alwaysRun = true)
+    public void begin() {
+
+        mainPage.goTo();
+        mainPage.maximize();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown(ITestResult iTestResult) {
+        mainPage.onTestFailure(iTestResult);
+        mainPage.quitDriver();
+    }
+}
