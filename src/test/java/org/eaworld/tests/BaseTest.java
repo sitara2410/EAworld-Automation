0f55611e897e4025794dd9911f8551f91a7e7bce
vamsi <vamsikrishna.kuppa@gmail.com>
2017-12-07 20:11:01 +0530

    Added BeforeTest and AfterTest for Browser init

--- src/test/java/org/eaworld/tests/BaseTest.java
+++ src/test/java/org/eaworld/tests/BaseTest.java
@@ -4,9 +4,7 @@ import com.google.inject.Inject;
 import org.eaworld.modules.DriverModule;
 import org.eaworld.pageimpl.MainPageImpl;
 import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Guice;
+import org.testng.annotations.*;
 
 @Guice(modules = {
         DriverModule.class
@@ -16,16 +14,20 @@ public class BaseTest {
     @Inject
     MainPageImpl mainPage;
 
-    @BeforeMethod(alwaysRun = true)
+    @BeforeTest(alwaysRun = true)
     public void begin() {
 
         mainPage.goTo();
         //mainPage.maximize();
     }
 
-    @AfterMethod(alwaysRun = true)
-    public void tearDown(ITestResult iTestResult) {
+    @AfterMethod
+    public void takeScreenshot(ITestResult iTestResult) {
         mainPage.onTestFailure(iTestResult);
+    }
+
+    @AfterTest
+    public void tearDown() {
         mainPage.quitDriver();
     }
 }
